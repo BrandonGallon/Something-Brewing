38ff2915ed23f8513457741e713b008e681df2a4
Ruben <Rubenhoogerdijk@gmail.com>
2025-11-19 13:47:24 +0100

    potion test

---
+++ SOMething Brewing/Assets/Scripts/FrostPotion.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class FrostPotion : Potion
+{
+
+    public override void ApplyEffect(GameObject Enemy)
+    {
+        base.ApplyEffect(Enemy);
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ SOMething Brewing/Assets/Scripts/SmokeBomb.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SmokeBomb : Potion
+{
+
+    public override void ApplyEffect(GameObject Enemy)
+    {
+        base.ApplyEffect(Enemy);
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
