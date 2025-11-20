95f2a4212f9c1e1efa4a539ff26571e2e611aad0
Ruben <Rubenhoogerdijk@gmail.com>
2025-11-20 13:42:58 +0100

    Scripty

--- SOMething Brewing/Assets/Scripts/PotionDragController.cs
+++ SOMething Brewing/Assets/Scripts/PotionDragController.cs
@@ -1,99 +1,99 @@
-using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Controls;
+//using UnityEngine;
+//using UnityEngine.InputSystem;
+//using UnityEngine.InputSystem.Controls;
 
-public class PotionDragController : MonoBehaviour
-{
-    private TouchControls controls;
-    private Camera cam;
-    private DraggablePotion currentPotion;
+//public class PotionDragController : MonoBehaviour
+//{
+//    private TouchControls controls;
+//    private Camera cam;
+//    private DraggablePotion currentPotion;
 
-    // Plane at table depth (adjust in Inspector)
-    public Transform dragPlane;
+//    // Plane at table depth (adjust in Inspector)
+//    public Transform dragPlane;
 
-    private Plane plane;
+//    private Plane plane;
 
-    private void Awake()
-    {
-        controls = new TouchControls();
-        cam = Camera.main;
-    }
+//    private void Awake()
+//    {
+//        controls = new TouchControls();
+//        cam = Camera.main;
+//    }
 
-    private void OnEnable()
-    {
-        controls.Enable();
+//    private void OnEnable()
+//    {
+//        controls.Enable();
 
-        // Create a plane from the dragPlane transform
-        plane = new Plane(dragPlane.forward, dragPlane.position);
-    }
+//        // Create a plane from the dragPlane transform
+//        plane = new Plane(dragPlane.forward, dragPlane.position);
+//    }
 
-    private void Update()
-    {
-        bool touching = controls.Touch.PrimaryTouchContact.ReadValue<float>() > 0.5f;
-        Vector2 screenPos = controls.Touch.PrimaryTouchPosition.ReadValue<Vector2>();
+//    private void Update()
+//    {
+//        bool touching = controls.Touch.PrimaryTouchContact.ReadValue<float>() > 0.5f;
+//        Vector2 screenPos = controls.Touch.PrimaryTouchPosition.ReadValue<Vector2>();
 
-        if (touching)
-        {
-            if (currentPotion == null)
-            {
-                TryPickUp(screenPos);
-            }
-            else
-            {
-                DragPotion(screenPos);
-            }
-        }
-        else
-        {
-            DropPotion();
-        }
-    }
+//        if (touching)
+//        {
+//            if (currentPotion == null)
+//            {
+//                TryPickUp(screenPos);
+//            }
+//            else
+//            {
+//                DragPotion(screenPos);
+//            }
+//        }
+//        else
+//        {
+//            DropPotion();
+//        }
+//    }
 
-    void TryPickUp(Vector2 screenPos)
-    {
-        Ray ray = cam.ScreenPointToRay(screenPos);
-        Debug.Log("Trying to pick up at screenPos: " + screenPos);
+//    void TryPickUp(Vector2 screenPos)
+//    {
+//        Ray ray = cam.ScreenPointToRay(screenPos);
+//        Debug.Log("Trying to pick up at screenPos: " + screenPos);
 
-        if (Physics.Raycast(ray, out RaycastHit hit))
-        {
-            Debug.Log("Hit: " + hit.collider.name);
+//        if (Physics.Raycast(ray, out RaycastHit hit))
+//        {
+//            Debug.Log("Hit: " + hit.collider.name);
 
-            currentPotion = hit.collider.GetComponent<DraggablePotion>();
+//            currentPotion = hit.collider.GetComponent<DraggablePotion>();
 
-            if (currentPotion != null)
-            {
-                Debug.Log("Picked up potion");
-                currentPotion.StartDragging();
-            }
-            else
-            {
-                Debug.Log("Object hit, but NOT draggable.");
-            }
-        }
-        else
-        {
-            Debug.Log("No collider hit.");
-        }
-    }
+//            if (currentPotion != null)
+//            {
+//                Debug.Log("Picked up potion");
+//                currentPotion.StartDragging();
+//            }
+//            else
+//            {
+//                Debug.Log("Object hit, but NOT draggable.");
+//            }
+//        }
+//        else
+//        {
+//            Debug.Log("No collider hit.");
+//        }
+//    }
 
 
-    void DragPotion(Vector2 screenPos)
-    {
-        Ray ray = cam.ScreenPointToRay(screenPos);
+//    void DragPotion(Vector2 screenPos)
+//    {
+//        Ray ray = cam.ScreenPointToRay(screenPos);
 
-        if (plane.Raycast(ray, out float distance))
-        {
-            Vector3 worldPoint = ray.GetPoint(distance);
-            currentPotion.transform.position = worldPoint;
-        }
-    }
+//        if (plane.Raycast(ray, out float distance))
+//        {
+//            Vector3 worldPoint = ray.GetPoint(distance);
+//            currentPotion.transform.position = worldPoint;
+//        }
+//    }
 
-    void DropPotion()
-    {
-        if (currentPotion != null)
-        {
-            currentPotion.StopDragging();
-            currentPotion = null;
-        }
-    }
-}
+//    void DropPotion()
+//    {
+//        if (currentPotion != null)
+//        {
+//            currentPotion.StopDragging();
+//            currentPotion = null;
+//        }
+//    }
+//}
